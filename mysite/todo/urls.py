a6d17f275adbdd2f6cba16c3b6b577d955824057
meia0407 <mei.ariga.0407@gmail.com>
2023-07-18 12:08:34 +0900

    見た目を向上させた

--- mysite/todo/urls.py
+++ mysite/todo/urls.py
@@ -1,18 +1,11 @@
 from django.urls import path
 from . import views
-<<<<<<< HEAD
 from .views import TodoList, TodoDetail, TodoCreate, TodoUpdate, TodoDelete
 
 urlpatterns = [
     path("", TodoList.as_view(), name="list"),
     path("detail/<int:pk>", TodoDetail.as_view(), name="detail"),
     path("create/", TodoCreate.as_view(), name="create"),
-    path("update/<int:pk>", TodoUpdate.as_view(), name="update"), 
-    path("delete/<int:pk>", TodoDelete.as_view(), name="delete"), 
-=======
-from .views import TodoList
-
-urlpatterns = [
-    path("", TodoList.as_view(), name="list"),
->>>>>>> 04f7a03bae29b79cf4e1a17a3ab241a394bc02cb
+    path("update/<int:pk>", TodoUpdate.as_view(), name="update"),
+    path("delete/<int:pk>", TodoDelete.as_view(), name="delete"),
 ]
--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -1,41 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-<<<<<<< HEAD
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-=======
-from django.views.generic import ListView
->>>>>>> 04f7a03bae29b79cf4e1a17a3ab241a394bc02cb
 
 from .models import Todo
 
 # Create your views here.
+
+
 def index(request):
     return HttpResponse("<h1>Hello World</h1>")
 
+
 class TodoList(ListView):
     model = Todo
-<<<<<<< HEAD
     context_object_name = "tasks"
-    
+
+
 class TodoDetail(DetailView):
     model = Todo
     context_object_name = "task"
+
 
 class TodoCreate(CreateView):
     model = Todo
     fields = "__all__"
     success_url = reverse_lazy("list")
-    
+
+
 class TodoUpdate(UpdateView):
     model = Todo
     fields = "__all__"
     success_url = reverse_lazy("list")
 
+
 class TodoDelete(DeleteView):
     model = Todo
     context_object_name = "task"
     success_url = reverse_lazy("list")
-=======
-    context_object_name = "tasks"
->>>>>>> 04f7a03bae29b79cf4e1a17a3ab241a394bc02cb

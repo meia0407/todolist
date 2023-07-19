e7ab0610c9a0e28460edc445a52b89246e91f653
meia0407 <mei.ariga.0407@gmail.com>
2023-07-19 13:34:17 +0900

    見た目を変更した

--- mysite/todo/models.py
+++ mysite/todo/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Todo(models.Model):
     title = models.CharField("授業名", max_length=30)
     assignment = models.TextField("課題", blank=True)
     description = models.TextField("詳細", blank=True)
     deadline = models.DateField("締め切り")
-    
+
     def __str__(self):
         return self.title
-    

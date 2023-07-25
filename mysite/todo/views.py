8e3cbe0f1e6f297f50e128e0f365a95e46a339b8
meia0407 <mei.ariga.0407@gmail.com>
2023-07-25 14:48:49 +0900

    不要なコードの削除

--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import Todo
 
 # Create your views here.
-
-
-def index(request):
-    return HttpResponse("<h1>Hello World</h1>")
 
 
 class TodoList(ListView):

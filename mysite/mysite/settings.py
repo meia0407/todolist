2ee904e268c85c272d9b6ec8a8135d36823a4943
meia0407 <mei.ariga.0407@gmail.com>
2024-01-31 15:50:55 +0900

    change settings.py for static files

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
-STATIC_ROOT = '/home/meia0407/meia0407.pythonanywhere.com/mysite/staticfiles'
+STATIC_ROOT = '/home/meia0407/meia0407.pythonanywhere.com/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
